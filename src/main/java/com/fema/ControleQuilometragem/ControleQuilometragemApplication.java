74ec9c8e7cb9d1f050cd6139fda569e77e3fb710
Hugo Ferreira da Silva <hugo.silva@casaavenida.com.br>
2025-05-12 17:07:15 -0300

    first commit

---
+++ src/main/java/com/fema/ControleQuilometragem/ControleQuilometragemApplication.java
@@ -0,0 +1,13 @@
+package com.fema.ControleQuilometragem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleQuilometragemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleQuilometragemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fema/ControleQuilometragem/ControleQuilometragemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fema.ControleQuilometragem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleQuilometragemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
