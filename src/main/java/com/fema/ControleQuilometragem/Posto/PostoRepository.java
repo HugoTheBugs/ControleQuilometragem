a4b462c3b39d362e99cba403ecdeb703441d5b96
Hugo Ferreira da Silva <hugo.silva@casaavenida.com.br>
2025-05-13 17:44:54 -0300

    feat: adiciona o PostoRepository

---
+++ src/main/java/com/fema/ControleQuilometragem/Posto/PostoRepository.java
@@ -0,0 +1,7 @@
+package com.fema.ControleQuilometragem.Posto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostoRepository extends JpaRepository<PostoModel, Long> {
+
+}
