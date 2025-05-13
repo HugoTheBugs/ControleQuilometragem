bd3d606dca32e16b9068c82dd7f1feff2c02048c
Hugo Ferreira da Silva <hugo.silva@casaavenida.com.br>
2025-05-13 16:30:00 -0300

    feat: arquivo padrão de commits

--- src/main/java/com/fema/ControleQuilometragem/ControleQuilometragemApplication.java
+++ src/main/java/com/fema/ControleQuilometragem/ControleQuilometragemApplication.java
@@ -8,6 +8,7 @@ public class ControleQuilometragemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControleQuilometragemApplication.class, args);
+		System.out.println("Hello World!");
 	}
 
 }
