fc82858ad560d1cc58f5dcb87e31e703f5fa3bf4
Hugo Ferreira da Silva <hugo.silva@casaavenida.com.br>
2025-05-14 17:38:12 -0300

    fix: corrige o pom, PostoController e application.properties

--- src/main/java/com/fema/ControleQuilometragem/Posto/PostoController.java
+++ src/main/java/com/fema/ControleQuilometragem/Posto/PostoController.java
@@ -1,13 +1,39 @@
 package com.fema.ControleQuilometragem.Posto;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/postos")
 public class PostoController {
 
-    @GetMapping("/boasvindas")
-    public String boasVindas(){
-        return "Essa é minha primeira mensagem nessa rota!";
+    private final PostoService postoService;
+
+    public PostoController(PostoService postoService){
+        this.postoService = postoService;
     }
+
+    //Adicionar Posto
+    @PostMapping("/criar")
+    public ResponseEntity<String> criarPosto(@RequestBody PostoDTO posto){
+        PostoDTO novoPosto = postoService.criarPosto(posto);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Posto criado com sucesso: " + novoPosto.getNome() + " (ID): " + novoPosto.getId());
+    }
+
+    //Mostrar todos os Postos
+    @GetMapping("/listar")
+    public ResponseEntity<List<PostoDTO>> listarPostos(){
+        List<PostoDTO> postos = postoService.listarPostos();
+        return ResponseEntity.ok(postos);
+    }
+
+    //Mostrar posto por ID
+
+    //Alterar dados do posto
+
+    //Deletar Posto
 }
