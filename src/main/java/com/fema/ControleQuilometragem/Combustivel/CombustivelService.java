27402f2d30cb8ab4afca5ec26870433b036e991b
Hugo Ferreira da Silva <hugo.silva@casaavenida.com.br>
2025-05-16 11:54:57 -0300

    fix: ajuste no CombustivelService e MarcaService

--- src/main/java/com/fema/ControleQuilometragem/Combustivel/CombustivelService.java
+++ src/main/java/com/fema/ControleQuilometragem/Combustivel/CombustivelService.java
@@ -2,6 +2,9 @@ package com.fema.ControleQuilometragem.Combustivel;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class CombustivelService {
     private CombustivelRepository combustivelRepository;
@@ -13,7 +16,17 @@ public class CombustivelService {
     }
 
     //Criar novo combustivel
+    public CombustivelDTO criarCombustivel(CombustivelDTO combustivelDTO){
+        CombustivelModel combustivel = combustivelMapper.map(combustivelDTO);
+        combustivel = combustivelRepository.save(combustivel);
+        return combustivelMapper.map(combustivel);
+    }
 
     //Listar combustiveis
-
+    public List<CombustivelDTO> listarCombustiveis(){
+        List<CombustivelModel> combustiveis = combustivelRepository.findAll();
+        return combustiveis.stream()
+                .map(combustivelMapper::map)
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/com/fema/ControleQuilometragem/Marca/MarcaService.java
+++ src/main/java/com/fema/ControleQuilometragem/Marca/MarcaService.java
@@ -2,6 +2,9 @@ package com.fema.ControleQuilometragem.Marca;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class MarcaService {
     public MarcaRepository marcaRepository;
@@ -13,6 +16,17 @@ public class MarcaService {
     }
 
     //Criar nova Marca
+    public MarcaDTO criarMarca(MarcaDTO marcaDTO){
+        MarcaModel marca = marcaMapper.map(marcaDTO);
+        marca = marcaRepository.save(marca);
+        return marcaMapper.map(marca);
+    }
 
     //Listar Marcas
+    public List<MarcaDTO> listarMarcas(){
+        List<MarcaModel> marcas = marcaRepository.findAll();
+        return marcas.stream()
+                .map(marcaMapper::map)
+                .collect(Collectors.toList());
+    }
 }
