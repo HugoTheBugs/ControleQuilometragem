4ebfe23d71157e2eaaf4458c9cba8cee0639a63a
Hugo Ferreira da Silva <hugo.silva@casaavenida.com.br>
2025-05-16 17:46:09 -0300

    fix: corrige MarcaModel

--- src/main/java/com/fema/ControleQuilometragem/Marca/MarcaModel.java
+++ src/main/java/com/fema/ControleQuilometragem/Marca/MarcaModel.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Table(name = "tb_marca")
 @Data
@@ -18,4 +20,5 @@ public class MarcaModel {
 
     @Column (name="nome")
     private String nome;
+
 }
