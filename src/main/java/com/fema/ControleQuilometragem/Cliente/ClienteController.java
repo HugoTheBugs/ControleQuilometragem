f03d82e881088f9fb3e46ac850f5c5d1db6129e2
Hugo Ferreira <hugoferreira2323@gmail.com>
2025-05-19 19:10:47 -0300

    fix: ajuste no ClienteController e ClienteService

--- src/main/java/com/fema/ControleQuilometragem/Cliente/ClienteController.java
+++ src/main/java/com/fema/ControleQuilometragem/Cliente/ClienteController.java
@@ -1,4 +1,33 @@
 package com.fema.ControleQuilometragem.Cliente;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
+
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService){
+        this.clienteService = clienteService;
+    }
+
+    //Adicionar Cliente
+    @PostMapping("/criar")
+    public ResponseEntity<String> criarCliente(@RequestBody ClienteDTO cliente){
+        ClienteDTO novoCliente = clienteService.criarCliente(cliente);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Cliente criado com sucesso: " + novoCliente.getNome() + " (ID): " + novoCliente.getId());
+    }
+
+    //Listar Clientes
+    @GetMapping("/listar")
+    public ResponseEntity<List<ClienteDTO>> listarClientes(){
+        List<ClienteDTO> clientes = clienteService.listarClientes();
+        return ResponseEntity.ok(clientes);
+    }
 }
--- src/main/java/com/fema/ControleQuilometragem/Cliente/ClienteService.java
+++ src/main/java/com/fema/ControleQuilometragem/Cliente/ClienteService.java
@@ -1,4 +1,32 @@
 package com.fema.ControleQuilometragem.Cliente;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class ClienteService {
+    public ClienteRepository clienteRepository;
+    public ClienteMapper clienteMapper;
+
+    public ClienteService(ClienteRepository clienteRepository, ClienteMapper clienteMapper){
+        this.clienteRepository = clienteRepository;
+        this.clienteMapper = clienteMapper;
+    }
+
+    //Criar Novo Cliente
+    public ClienteDTO criarCliente(ClienteDTO clienteDTO){
+        ClienteModel cliente = clienteMapper.map(clienteDTO);
+        cliente = clienteRepository.save(cliente);
+        return clienteMapper.map(cliente);
+    }
+
+    //Listar Clientes
+    public List<ClienteDTO> listarClientes(){
+        List<ClienteModel> clientes = clienteRepository.findAll();
+        return clientes.stream()
+                .map(clienteMapper::map)
+                .collect(Collectors.toList());
+    }
 }
