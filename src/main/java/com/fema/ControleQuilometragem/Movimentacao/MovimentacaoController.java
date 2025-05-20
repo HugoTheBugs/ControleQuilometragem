fe03c127e69e501c54d979a4f8d44a526bbff5ad
Hugo Ferreira <hugoferreira2323@gmail.com>
2025-05-20 19:17:00 -0300

    feat: cria MovimentacaoController, MovimentacaoService

---
+++ src/main/java/com/fema/ControleQuilometragem/Movimentacao/MovimentacaoController.java
@@ -0,0 +1,32 @@
+package com.fema.ControleQuilometragem.Movimentacao;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movimentacoes")
+public class MovimentacaoController {
+
+    private final MovimentacaoService movimentacaoService;
+
+    public MovimentacaoController(MovimentacaoService movimentacaoService){
+        this.movimentacaoService = movimentacaoService;
+    }
+
+    @PostMapping("/criar")
+    public ResponseEntity<String> criarLancamento(@RequestBody MovimentacaoDTO movimentacao){
+        MovimentacaoDTO novaMovimentacao = movimentacaoService.criarLancamento(movimentacao);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Movimentação criada com sucesso, (ID): " + novaMovimentacao.getId());
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<MovimentacaoDTO>> listarMovimentacao(){
+        List<MovimentacaoDTO> movimentacao = movimentacaoService.listarMovimentacao();
+        return ResponseEntity.ok(movimentacao);
+    }
+}
---
+++ src/main/java/com/fema/ControleQuilometragem/Movimentacao/MovimentacaoService.java
@@ -0,0 +1,34 @@
+package com.fema.ControleQuilometragem.Movimentacao;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class MovimentacaoService {
+
+    private final MovimentacaoRepository movimentacaoRepository;
+    private final MovimentacaoMapper movimentacaoMapper;
+
+    public MovimentacaoService(MovimentacaoRepository movimentacaoRepository, MovimentacaoMapper movimentacaoMapper){
+        this.movimentacaoRepository = movimentacaoRepository;
+        this.movimentacaoMapper = movimentacaoMapper;
+    }
+
+    //Realizar Lancamento
+    public MovimentacaoDTO criarLancamento(MovimentacaoDTO movimentacaoDTO){
+        MovimentacaoModel movimentacao = movimentacaoMapper.map(movimentacaoDTO);
+        movimentacao = movimentacaoRepository.save(movimentacao);
+        return movimentacaoMapper.map(movimentacao);
+    }
+
+    //Listar Movimentação
+    public List<MovimentacaoDTO> listarMovimentacao(){
+        List<MovimentacaoModel> movimentacao = movimentacaoRepository.findAll();
+        return movimentacao.stream()
+                .map(movimentacaoMapper::map)
+                .collect(Collectors.toList());
+    }
+
+}
