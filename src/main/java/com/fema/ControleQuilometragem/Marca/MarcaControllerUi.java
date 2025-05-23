7eb0537393d7346a359681238b66f35624bbd16d
Hugo Ferreira da Silva <hugo.silva@casaavenida.com.br>
2025-05-23 17:51:00 -0300

    fix: ajustes de comunicação entre MarcaControllerUi e listarMarcas.html

--- src/main/java/com/fema/ControleQuilometragem/Marca/MarcaControllerUi.java
+++ src/main/java/com/fema/ControleQuilometragem/Marca/MarcaControllerUi.java
@@ -1,8 +1,12 @@
 package com.fema.ControleQuilometragem.Marca;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -21,5 +25,12 @@ public class MarcaControllerUi {
         List<MarcaDTO> marcas = marcaService.listarMarcas();
         model.addAttribute("marcas", marcas);
         return "listarMarcas";
+    }
+
+    // Deletar Marca
+    @GetMapping("/deletar/{id}")
+    public String deletarMarcaPorId(@PathVariable Long id){
+        marcaService.deletarMarcaPorId(id);
+        return "redirect:/marcas/ui/listar";
     }
 }
