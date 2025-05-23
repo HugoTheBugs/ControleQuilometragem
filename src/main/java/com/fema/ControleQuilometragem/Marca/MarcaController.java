8a195ea1a9af300d88902f6cc600d8723d535cca
Hugo Ferreira da Silva <hugo.silva@casaavenida.com.br>
2025-05-23 16:21:07 -0300

    feat: adiciona os métodos de alterar por id, listar por id e deletar por id no MarcaController

--- src/main/java/com/fema/ControleQuilometragem/Marca/MarcaController.java
+++ src/main/java/com/fema/ControleQuilometragem/Marca/MarcaController.java
@@ -29,4 +29,42 @@ public class MarcaController {
         List<MarcaDTO> marcas = marcaService.listarMarcas();
         return ResponseEntity.ok(marcas);
     }
+
+    // Mostrar marca por id
+    @GetMapping("/listar/{id}") //path variable
+    public ResponseEntity<?> listarMarcasPorId(@PathVariable Long id){
+
+        MarcaDTO marca = marcaService.listarMarcasPorId(id);
+        if(marca != null){
+            return ResponseEntity.ok(marca);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Marca com o id: " + id + " não existe nos nossos registros!");
+        }
+    }
+
+    // Deletar Marca
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<String> deletarMarcaPorId(@PathVariable Long id){
+        if(marcaService.listarMarcasPorId(id) !=null){
+            marcaService.deletarMarcaPorId(id);
+            return ResponseEntity.ok("Marca com o ID " + id + " deletado com sucesso!");
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("A marca com o id " + id + " não encontrado!");
+        }
+    }
+
+    // Alterar dados das Marcas
+    @PutMapping("/alterar/{id}")
+    public ResponseEntity<?> alterarMarcaPorId(@PathVariable Long id, @RequestBody MarcaDTO marcaAtualizado){
+        MarcaDTO marca = marcaService.atualizarMarca(id, marcaAtualizado);
+
+        if(marca != null){
+            return ResponseEntity.ok(marca);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Marca com o id: " + id + " não existe nos nossos registros!");
+        }
+    }
 }
