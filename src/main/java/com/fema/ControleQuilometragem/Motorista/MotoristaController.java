91f70f4e4d7e604d75e4fb645248178d74f51831
Hugo Ferreira <hugoferreira2323@gmail.com>
2025-05-25 18:20:29 -0300

    feat: remocao do ControllerUi e listarMarcas.html

--- src/main/java/com/fema/ControleQuilometragem/Motorista/MotoristaController.java
+++ src/main/java/com/fema/ControleQuilometragem/Motorista/MotoristaController.java
@@ -1,5 +1,4 @@
 package com.fema.ControleQuilometragem.Motorista;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,5 +27,43 @@ public class MotoristaController {
     public ResponseEntity<List<MotoristaDTO>> listarMotoristas(){
         List<MotoristaDTO> motoristas = motoristaService.listarMotoristas();
         return ResponseEntity.ok(motoristas);
+    }
+
+    // Mostrar motorista por id
+    @GetMapping("/listar/{id}") //path variable
+    public ResponseEntity<?> listarMotoristasPorId(@PathVariable Long id){
+
+        MotoristaDTO motorista = motoristaService.listarMotoristasPorId(id);
+        if(motorista != null){
+            return ResponseEntity.ok(motorista);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Motorista com o id: " + id + " não existe nos nossos registros!");
+        }
+    }
+
+    // Deletar Motorista
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<String> deletarMotoristaPorId(@PathVariable Long id){
+        if(motoristaService.listarMotoristasPorId(id) !=null){
+            motoristaService.deletarMotoristaPorId(id);
+            return ResponseEntity.ok("Motorista com o ID " + id + " deletado com sucesso!");
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("O motorista com o id " + id + " não encontrado!");
+        }
+    }
+
+    // Alterar dados dos Motoristas
+    @PutMapping("/alterar/{id}")
+    public ResponseEntity<?> alterarMotoristaPorId(@PathVariable Long id, @RequestBody MotoristaDTO motoristaAtualizado){
+        MotoristaDTO motorista = motoristaService.atualizarMotorista(id, motoristaAtualizado);
+
+        if(motorista != null){
+            return ResponseEntity.ok(motorista);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Motorista com o id: " + id + " não existe nos nossos registros!");
+        }
     }
 }
